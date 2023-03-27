9dcab0f9b69ee6f568d8c710f31d46dee608a3fb
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-03-27 15:52:03 -0700

    Finishes the homework assignemtn

---
+++ src/main/java/array/homework.java
@@ -0,0 +1,58 @@
+package array;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class homework {
+    public static void main(String[] args) {
+        // Read a line of text such as “I love Java. You love Java. She loves Java.”
+        // Split them into words
+        // Count number of times each word (excluding dot .) occurs
+        // So the end result is something like following:
+        // "I" occurs once
+        // "love" occurs twice
+        // "Java" occurs 3 times
+        // "You" occurs once
+        // "She" occurs once
+        // "loves" occurs once
+
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("Enter a line of text: ");
+            String line = scan.nextLine();
+            System.out.println();
+
+            String[] words = line.replace(".", "").split(" ");
+
+            HashMap<String, Integer> map = findOccurances(words);
+
+            printOccurances(map);
+
+        }
+
+    }
+
+    private static HashMap<String, Integer> findOccurances(String[] words) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
+            }
+        }
+        return map;
+    }
+
+    private static void printOccurances(HashMap<String, Integer> map) {
+        for (String word : map.keySet()) {
+            if (map.get(word) == 1) {
+                System.out.println(word + " occurs once");
+            } else if (map.get(word) == 2) {
+                System.out.println(word + " occurs twice");
+            } else if (map.get(word) >= 3) {
+                System.out.println(word + " occurs 3 times");
+            }
+        }
+    }
+}
