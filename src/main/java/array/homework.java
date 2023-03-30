a18518282099eeee77fbe3eca94e311cfd71e1ba
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-03-30 07:48:45 -0700

    Adds pseudocode

--- src/main/java/array/homework.java
+++ src/main/java/array/homework.java
@@ -21,35 +21,45 @@ public class homework {
 
     // helper method to get unique words
     private static String[] getUniqueWords(String[] words) {
+        // Creating an array of strings with the same length as the words array.
         String[] uniqueWords = new String[words.length];
+        // A counter for the number of unique words.
         int count = 0;
+        // Checking if the word is in the array.
         for (int i = 0; i < words.length; i++) {
             if (!isWordInArray(words[i], uniqueWords)) {
                 uniqueWords[count] = words[i];
                 count++;
             }
         }
+        // Creating a new array with the length of the number of unique words.
         String[] result = new String[count];
+        // Copying the unique words from the uniqueWords array to the result array.
         for (int i = 0; i < count; i++) {
             result[i] = uniqueWords[i];
         }
+        // Returning the array of unique words.
         return result;
     }
 
     // helper method to check if a word is in an array
     private static boolean isWordInArray(String word, String[] array) {
+        // Checking if the word is in the array.
         for (int i = 0; i < array.length; i++) {
             if (word.equals(array[i])) {
                 return true;
             }
         }
+        // Returning the value false.
         return false;
     }
 
     // helper method to get counts
     private static int[] getCounts(String[] uniqueWords, String[] words) {
+        // Creating an array of integers with the same length as the uniqueWords array.
         int[] counts = new int[uniqueWords.length];
 
+        // Counting the number of times a word appears in the text.
         for (int i = 0; i < uniqueWords.length; i++) {
             for (int j = 0; j < words.length; j++) {
                 if (uniqueWords[i].equals(words[j])) {
@@ -57,11 +67,13 @@ public class homework {
                 }
             }
         }
+        // Returning the array of counts.
         return counts;
     }
 
     // helper method to print results
     private static void printOccurances(String[] uniqueWords, int[] counts) {
+        // Printing the results.
         for (int i = 0; i < uniqueWords.length; i++) {
             if (counts[i] == 1) {
                 System.out.printf("\"%s\" occurs once\n", uniqueWords[i]);
