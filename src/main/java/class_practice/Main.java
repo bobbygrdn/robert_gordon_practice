30943bd6b7a7e26747cf5060d6f4efa809b1e4fc
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-03-30 11:55:17 -0700

    Adds a static method and another instance method

--- src/main/java/class_practice/Main.java
+++ src/main/java/class_practice/Main.java
@@ -4,10 +4,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Calling the static method bankName from the ATM class.
+        ATM.bankName("JPMorgan & Chase");
+
         // Create an ATM object using the ATM class
         ATM atm = new ATM("John", "1234", 1000, 1234);
+
+        // Calling the userInfo instance method from the ATM class.
+        atm.userInfo();
+
+        // Creating a new scanner object called scan.
         try (Scanner scan = new Scanner(System.in)) {
 
+            // Setting the boolean variable loggedIn to false.
             boolean loggedIn = false;
 
             // Prompt user to login
@@ -17,16 +27,21 @@ public class Main {
                 loggedIn = atm.login(pin);
             }
 
+            // This is just a welcome message for the user.
             if (loggedIn == true) {
                 System.out.printf("Welcome %s!\n", atm.getName());
-
             }
 
+            // Checking if the user is logged in.
             while (loggedIn == true) {
 
+                // Calling the startMenu method from the ATM class.
                 atm.startMenu();
+
+                // Getting the user's input and storing it in the variable choice.
                 int choice = scan.nextInt();
 
+                // A switch statement that is used to determine what the user wants to do.
                 switch (choice) {
                     case 1:
                         System.out.println("How much would you like to withdraw?");
