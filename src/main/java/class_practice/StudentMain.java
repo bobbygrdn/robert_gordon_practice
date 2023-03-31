4488fc018c299ba320d8565164ebff0dab4295fe
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-03-31 08:34:44 -0700

    creates a studentMain file

---
+++ src/main/java/class_practice/StudentMain.java
@@ -0,0 +1,17 @@
+package class_practice;
+
+public class StudentMain {
+    public static void main(String[] args) {
+        StudentRecord student1 = new StudentRecord("Charles", 90.5);
+
+        student1.setScienceScore(80.2);
+        student1.setAlgebraScore(70.1);
+
+        StudentRecord student2 = new StudentRecord("Michael", 80.9, 60.9, 75.8);
+
+        System.out.println(StudentRecord.getStudentCount());
+
+        student1.print(student1.getName(), student1.getAverageScore());
+        student2.print(student2.getName(), student2.getAverageScore());
+    }
+}
