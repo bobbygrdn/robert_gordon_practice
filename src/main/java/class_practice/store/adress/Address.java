105e7148778211c613f66a4c65046b1923cb10ea
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-03 11:23:44 -0700

    creates address class in adress subpackage

---
+++ src/main/java/class_practice/store/adress/Address.java
@@ -0,0 +1,10 @@
+package class_practice.store.adress;
+
+public class Address {
+    private boolean isPrimaryAdress;
+    private int streetNumber;
+    private String streetName;
+    private String city;
+    private String state;
+
+}
