a9ac2d123a21acd5aabd63754ed04badbb768096
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-03 13:38:49 -0700

    creates hospital main class

---
+++ src/main/java/class_practice/inheritanceDemo/Hospital.java
@@ -0,0 +1,9 @@
+package class_practice.inheritanceDemo;
+
+public class Hospital {
+    public static void main(String[] args) {
+        Surgeon surgeon = new Surgeon();
+        surgeon.Doctor_Details();
+        surgeon.Surgeon_Details();
+    }
+}
