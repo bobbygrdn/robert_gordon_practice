a5fd7568d07829af27acc8ed58cc693c13c1140c
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-03 13:38:35 -0700

    creates doctor class

---
+++ src/main/java/class_practice/inheritanceDemo/Doctor.java
@@ -0,0 +1,12 @@
+package class_practice.inheritanceDemo;
+
+public class Doctor {
+
+    String DoctorName;
+    String Department;
+
+    public void Doctor_Details() {
+        System.out.println("Doctor Details...");
+    }
+
+}
