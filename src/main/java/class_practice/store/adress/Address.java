9eb59c1bbdf518497e7dd9ffbcb8476d58c88f12
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-03 11:45:37 -0700

    creates the address class under the address subpackage

--- src/main/java/class_practice/store/adress/Address.java
+++ src/main/java/class_practice/store/adress/Address.java
@@ -7,4 +7,52 @@ public class Address {
     private String city;
     private String state;
 
+    public Address(boolean isPrimaryAdress, int streetNumber, String streetName, String city, String state) {
+        this.isPrimaryAdress = isPrimaryAdress;
+        this.streetNumber = streetNumber;
+        this.streetName = streetName;
+        this.city = city;
+        this.state = state;
+    }
+
+    public boolean isPrimaryAdress() {
+        return isPrimaryAdress;
+    }
+
+    public void setPrimaryAdress(boolean isPrimaryAdress) {
+        this.isPrimaryAdress = isPrimaryAdress;
+    }
+
+    public int getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(int streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
 }
