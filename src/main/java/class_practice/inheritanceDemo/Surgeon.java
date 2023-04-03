c286d29e722e4b513fc2f031089b9e73ad70617e
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-03 13:38:41 -0700

    creates surgeon class

---
+++ src/main/java/class_practice/inheritanceDemo/Surgeon.java
@@ -0,0 +1,8 @@
+package class_practice.inheritanceDemo;
+
+public class Surgeon extends Doctor {
+    void Surgeon_Details() {
+        System.out.println("Surgeon  Detail...");
+        System.out.println(Department = "Cardio");
+    }
+}
