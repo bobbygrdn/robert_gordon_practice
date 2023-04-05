62f58427d4aafcfea7ff942068bcbf1c4677f98d
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-05 14:09:46 -0700

    creates a customer name class

---
+++ src/main/java/class_practice/store/CustomerName.java
@@ -0,0 +1,42 @@
+package class_practice.store;
+
+public class CustomerName {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+
+    public CustomerName(String firstName, String middleName, String lastName) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+    }
+
+    public void printName() {
+        System.out.printf("Customer Name: %s %s %s\n", firstName, middleName, lastName);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
