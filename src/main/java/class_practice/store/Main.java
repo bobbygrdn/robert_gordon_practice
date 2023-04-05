dfca675c6d12c82a783cc0bd23f4e6a18c8b3898
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-05 14:10:29 -0700

    updates logic to use customerName class

--- src/main/java/class_practice/store/Main.java
+++ src/main/java/class_practice/store/Main.java
@@ -7,14 +7,17 @@ public class Main {
         Product product1 = new Product("Pen", 2.99);
         Product product2 = new Product("Pencil", 1.99);
 
-        Customer customer1 = new Customer("John", "j@j.com",
+        Customer customer1 = new Customer("j@j.com",
                 new Product[] { product1, product2 });
+        CustomerName customer1Name = new CustomerName("John", "Doe", "Smith");
 
         Address customer1Address = new Address(true, 125, "Main street", "New York City", "New York");
         Address customer2Address = new Address(false, 215, "Second street", "New York City", "New York");
 
         customer1.addAddress(customer1Address);
         customer1.addAddress(customer2Address);
+
+        customer1Name.printName();
         customer1.getCustomerInfo();
     }
 }
