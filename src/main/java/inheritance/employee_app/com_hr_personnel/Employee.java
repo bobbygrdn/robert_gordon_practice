2dfb4771f60cc663f124b326f913f28cf5185c91
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-07 08:50:15 -0700

    creates employee parent class

---
+++ src/main/java/inheritance/employee_app/com_hr_personnel/Employee.java
@@ -0,0 +1,45 @@
+package inheritance.employee_app.com_hr_personnel;
+
+import java.time.LocalDate;
+
+public class Employee {
+    private String name;
+    private LocalDate hireDate;
+
+    public Employee(String name, LocalDate hireDate) {
+        this.name = name;
+        this.hireDate = hireDate;
+    }
+
+    public String getEmployeeInfo() {
+        return "Name: " + name + ", Hire Date: " + hireDate;
+    }
+
+    public String work() {
+        return name + " worked.";
+    }
+
+    public int computeNumberOfYearsWorkedSinceHired() {
+        int years = 0;
+        LocalDate currentDate = LocalDate.now();
+        years = currentDate.getYear() - hireDate.getYear();
+        return years;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(LocalDate hireDate) {
+        this.hireDate = hireDate;
+    }
+
+}
