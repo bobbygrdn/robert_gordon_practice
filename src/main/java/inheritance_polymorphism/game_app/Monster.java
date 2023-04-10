5c1f92c99a21ff0e48e75a2aeb7aba02a9667a37
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-10 13:08:00 -0700

    Creates monster parent class

---
+++ src/main/java/inheritance_polymorphism/game_app/Monster.java
@@ -0,0 +1,13 @@
+package inheritance_polymorphism.game_app;
+
+public class Monster {
+    private String name;
+
+    public Monster(String name) {
+        this.name = name;
+    }
+
+    public String attack() {
+        return "!^_&^$@+%$* I don't know how to attack!";
+    }
+}
