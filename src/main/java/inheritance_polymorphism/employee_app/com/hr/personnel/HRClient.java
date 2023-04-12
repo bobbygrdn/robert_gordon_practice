711b81e22dedb9cf8d738c4d2e485b3e883ea1ef
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-12 14:37:49 -0700

    creates a commissioned employee, calls methods accordingly and changes tenure print statements

--- src/main/java/inheritance_polymorphism/employee_app/com/hr/personnel/HRClient.java
+++ src/main/java/inheritance_polymorphism/employee_app/com/hr/personnel/HRClient.java
@@ -6,30 +6,43 @@ public class HRClient {
     public static void main(String[] args) {
         Department department = new Department("I.T.", "Sunnyvale, California");
 
-        SalariedEmployee employee1 = new SalariedEmployee("John", LocalDate.of(2022, 10, 25), 5000.0);
-        SalariedEmployee employee2 = new SalariedEmployee("Mary", LocalDate.of(2023, 02, 10), 6000.0);
-        HourlyEmployee employee3 = new HourlyEmployee("Charles", LocalDate.of(2021, 07, 18), 8, 2000.0);
+        SalariedEmployee employee1 = new SalariedEmployee("John", LocalDate.of(2022, 10, 25), 7000.0);
+        SalariedEmployee employee2 = new SalariedEmployee("Mary", LocalDate.of(2023, 02, 10), 8000.0);
+        HourlyEmployee employee3 = new HourlyEmployee("Charles", LocalDate.of(2021, 07, 18), 120, 45.0);
+        CommissionedEmployee employee4 = new CommissionedEmployee("Michael", LocalDate.of(2022, 06, 13), 0.5,
+                new double[] { 5000.0, 6000.0 });
 
         department.addEmployee(employee1);
         department.addEmployee(employee2);
         department.addEmployee(employee3);
+        department.addEmployee(employee4);
 
         String employee1Info = employee1.getEmployeeInfo();
         String employee2Info = employee2.getEmployeeInfo();
         String employee3Info = employee3.getEmployeeInfo();
+        String employee4Info = employee4.getEmployeeInfo();
 
         System.out.println(employee1Info);
         System.out.println(employee2Info);
         System.out.println(employee3Info);
+        System.out.println(employee4Info);
 
         int employee1Tenure = employee1.computeNumberOfYearsWorkedSinceHired();
         int employee2Tenure = employee2.computeNumberOfYearsWorkedSinceHired();
         int employee3Tenure = employee3.computeNumberOfYearsWorkedSinceHired();
+        int employee4Tenure = employee4.computeNumberOfYearsWorkedSinceHired();
 
-        System.out.printf("%s has been working for our company for %d years.\n", employee1.getName(), employee1Tenure);
-        System.out.printf("%s has been working for our company for %d years.\n", employee2.getName(), employee2Tenure);
-        System.out.printf("%s has been working for our company for %d years.\n", employee3.getName(), employee3Tenure);
+        System.out.println();
+        System.out.printf("%s has been working for our company for %d %s.\n", employee1.getName(), employee1Tenure,
+                employee1Tenure == 1 ? "year" : "years");
+        System.out.printf("%s has been working for our company for %d %s.\n", employee2.getName(), employee2Tenure,
+                employee2Tenure == 1 ? "year" : "years");
+        System.out.printf("%s has been working for our company for %d %s.\n", employee3.getName(), employee3Tenure,
+                employee3Tenure == 1 ? "year" : "years");
+        System.out.printf("%s has been working for our company for %d %s.\n", employee4.getName(), employee4Tenure,
+                employee4Tenure == 1 ? "year" : "years");
 
+        System.out.println();
         int departmentInfo = department.letEmployeesWorkAndReturnNumberOfEmployeesWhoWorked();
         System.out.printf("We have %d employees working in the %s department at our %s location.\n", departmentInfo,
                 department.getName(), department.getLocation());
