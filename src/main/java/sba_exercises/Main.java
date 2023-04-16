ba49f0c155072ebb995b0be5764001118d7818db
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-16 12:08:27 -0700

    Updates logic to test all methods in MySystem class, Item class and appropriate print statements for asthetic

--- src/main/java/sba_exercises/Main.java
+++ src/main/java/sba_exercises/Main.java
@@ -11,36 +11,59 @@ public class Main {
             File file = new File("sample.txt");
 
             Scanner scanner = new Scanner(file);
-
+            System.out.println("----------- Testing reading sample.txt -----");
+            System.out.printf("%-20s%-20s%-10s%-10s\n", "Name", "Description", "Price", "Quantity");
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String[] words = line.split("  ");
 
-                System.out.printf("item Name %s\nitem Description %s\nitem Price %s\nitem AvailableQuantity %s\n",
-                        words[0], words[1], words[2], words[3]);
+                System.out.printf("%-20s%-20s%-10.2f%-10d",
+                        words[0], words[1], Double.parseDouble(words[2]), Integer.parseInt(words[3]));
                 System.out.println();
 
             }
+            System.out.println();
             scanner.close();
-            System.out.println("----------------\n");
         } catch (FileNotFoundException e) {
         }
 
+        Item item1 = new Item("Coffee", "Iced Coffee", 1.00, 10);
+        System.out.println("----------- Testing the constructor of Item class -----");
+        System.out.printf("%-20s%-20s%-10.2f%-10d%-10d\n", item1.getItemName(), item1.getItemDescription(),
+                item1.getPrice(),
+                item1.getQuantity(), item1.getAvailableQuantity());
+
+        System.out.println("----------- Testing the constructor of MySystem class -----");
         MySystem newSystem = new MySystem();
 
         newSystem.printCurrentItems();
 
-        newSystem.printCurrentLength();
+        System.out.println("----------- Testing the addItem(...) method of MySystem class -----");
 
-        newSystem.addItem(new Item("Coffee", "Coffee", 1.00, 10));
+        System.out.printf("size before adding a new item = %d\n", newSystem.getMyItemsInHashMap().size());
 
-        newSystem.printCurrentLength();
+        newSystem.addItem(new Item("coffee", "coffee", 3.00, 10));
+
+        System.out.printf("size after adding a new item = %d\n", newSystem.getMyItemsInHashMap().size());
 
         newSystem.printCurrentItems();
 
         newSystem.addItem(new Item("salad", "cobb salad", 15.5, 12));
 
-        newSystem.printCurrentLength();
+        System.out.printf("size after adding a existing item = %d\n", newSystem.getMyItemsInHashMap().size());
+
+        System.out.println("----------- Testing the removeItem(...) method of MySystem class -----");
+        newSystem.removeItem("pizza");
+        System.out.printf("size after removing an item = %d\n", newSystem.getMyItemsInHashMap().size());
+
+        newSystem.printCurrentItems();
+
+        System.out.println("----------- Testing reduceAvailableQuantity(..) method -----");
+        Integer coffeeAvailableQuantity = newSystem.getMyItemsInHashMap().get("coffee").getAvailableQuantity();
+        System.out.println("coffee available quantity before purchase = " + coffeeAvailableQuantity);
+        newSystem.reduceAvailableQuantity("coffee");
+        coffeeAvailableQuantity = newSystem.getMyItemsInHashMap().get("coffee").getAvailableQuantity();
+        System.out.println("coffee available quantity after purchase = " + coffeeAvailableQuantity);
 
     }
 }
