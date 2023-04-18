5801ef5094dbae5ad5d2ae2ea5c809cd33839c6a
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-18 12:17:47 -0700

    adds standard deduction field and computeStandardEmployeeMonthlyDeduction

--- src/main/java/inheritance_polymorphism/employee_app/gov/irs/TaxPayer.java
+++ src/main/java/inheritance_polymorphism/employee_app/gov/irs/TaxPayer.java
@@ -4,6 +4,11 @@ public interface TaxPayer {
     public static final double HOURLY_TAX_RATE = 0.25;
     public static final double SALARIED_TAX_RATE = 0.30;
     public static final double CORP_TAX_RATE = 0.20;
+    public static final double DEFAULT_STANDARD_EMPLOYEE_MONTHLY_DEDUCTION = 250.0;
 
     double computeMonthlyTaxToPay();
+
+    default double computeStandardEmployeeMonthlyDeduction() {
+        return DEFAULT_STANDARD_EMPLOYEE_MONTHLY_DEDUCTION;
+    }
 }
