ee240bf974829945ec5199a9d7bb026e237ec757
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-19 07:48:50 -0700

    Creates main class that tests myOperation class by calling all methods and logic to test user input with scanner

---
+++ src/main/java/sba_core_java/exercise1/Main.java
@@ -0,0 +1,37 @@
+package sba_core_java.exercise1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] myStringArrayTestValues = { "life", "is", "worth", "living", "with", "passion" };
+
+        MyOperation myOperation = new MyOperation();
+
+        myOperation.convertStringArrayIntoArrayList(myStringArrayTestValues);
+
+        System.out.println(myOperation.myArrayList.toString());
+
+        myOperation.replaceAnElementInTheStringArrayListGiven(1);
+
+        System.out.println(myOperation.myArrayList.toString());
+
+        ArrayList<String> newArrayList = myOperation.createANewArrayListFromExistingArray();
+
+        System.out.println(newArrayList.toString());
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter an integer: ");
+            if (scanner.hasNextInt()) {
+                Integer newInt = scanner.nextInt();
+                System.out.println(newInt);
+            } else {
+                throw new MyOwnException("Invalid integer");
+            }
+        } catch (MyOwnException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
